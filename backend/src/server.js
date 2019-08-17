9ccce15aca924434056fcab1c86acc332fbd35e4
Léo Viggiano <leoviggiano1@outlook.com>
2019-08-16 21:57:20 -0300

    Connect MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,11 @@ const routes = require("./routes");
 const cors = require("cors");
 const server = express();
 
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-2e8eu.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
 server.use(cors());
 server.use(express.json());
 server.use(routes);
