d3ba58a13ec24a10ef7288cf4770b31e16260d6e
Léo Viggiano <leoviggiano1@outlook.com>
2019-08-16 22:29:48 -0300

    Rotas GET e POST inseridas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,12 @@
+const DevController = require("./controllers/DevController");
+const DislikeController = require("./controllers/DislikeController");
+const LikeController = require("./controllers/LikeController");
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Teste" });
-});
+routes.get("/devs", DevController.index);
+routes.post("/devs", DevController.store);
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DislikeController.store);
 
 module.exports = routes;
