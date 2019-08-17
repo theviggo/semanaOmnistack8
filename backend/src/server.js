227e07497924eda578a0722ec50201ece731e398
Léo Viggiano <leoviggiano1@outlook.com>
2019-08-16 21:54:29 -0300

    Conexao com o servidor

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
