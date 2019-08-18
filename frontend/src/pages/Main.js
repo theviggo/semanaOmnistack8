9b4532f9ef85e18f1daf1423f4b79df736ec30e1
Léo Viggiano <leoviggiano1@outlook.com>
2019-08-18 19:00:30 -0300

    Página Main estilizada e usando o GET

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./Main.css";
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import api from "../services/api";
 
-export default function Main({ match }){
-  return(
-    <h1>
-      {match.params.id}
-    </h1>
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
+        }
+      });
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt="" />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className="buttons">
+              <button type="button">
+                <img src={like} alt="like" />
+              </button>
+              <button type="button">
+                <img src={dislike} alt="dislike" />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
